a2cee345c187d706a6b3f6d058365805a667be7c
fbudin69500 <fbudin@unc.edu>
2014-10-10 11:42:52 -0400

    ENH: convert option can convert files containing multiple transforms
    Taking full advantage of the new implementation of ITKTransformFileReaderTemplate and ITKTransformFileWriterTemplate. The type of the input transform does not matter, and does not have to be specified. New flags have been therefore added ('2f' and '2d') but old flags ('d2f' and f2d') have been kept for backward compatibility purposes. Any transform type should also be supported now.

--- ConvertTransform.cpp
+++ ConvertTransform.cpp
@@ -8,6 +8,29 @@
 #include "ConvertTransform.h"
 #include "itkVersion.h"
 
+
+#if (ITK_VERSION_MAJOR == 4 && ITK_VERSION_MINOR >= 5) || ITK_VERSION_MAJOR> 4
+template< class U >
+int ConvertTransforms( std::string input , std::string output )
+{
+
+  typename itk::TransformFileReaderTemplate< U >::Pointer transformFile =
+  itk::TransformFileReaderTemplate< U >::New() ;
+  transformFile->SetFileName( input.c_str() ) ;
+  transformFile->Update() ;
+
+  typename itk::TransformFileWriterTemplate< U >::Pointer transformWriter =
+   itk::TransformFileWriterTemplate< U >::New() ;
+  transformWriter->SetFileName( output.c_str() ) ;
+  while( !transformFile->GetTransformList()->empty() )
+  {
+    transformWriter->AddTransform( transformFile->GetTransformList()->front() ) ;
+    transformFile->GetTransformList()->pop_front() ;
+  }
+  transformWriter->Update() ;
+  return 0 ;
+}
+#else
 template< class S , class T , class V >
 int ConvertType( typename S::Pointer inputTransform , std::string output )
 {
@@ -30,13 +53,7 @@ int ConvertType( typename S::Pointer inputTransform , std::string output )
      outputParam.SetElement( i , static_cast< V >( inputParam.GetElement( i ) ) ) ;
   }
   outputTransform->SetFixedParameters( outputParam ) ;
-  #if (ITK_VERSION_MAJOR == 4 && ITK_VERSION_MINOR >= 5) || ITK_VERSION_MAJOR> 4
-  typename itk::TransformFileWriterTemplate< V >::Pointer transformWriter =
-     itk::TransformFileWriterTemplate< V >::New() ;
-  #else
   itk::TransformFileWriter::Pointer transformWriter = itk::TransformFileWriter::New();
-  #endif
-
   transformWriter->SetFileName( output.c_str() ) ;
   transformWriter->AddTransform( outputTransform ) ;
   transformWriter->Update() ;
@@ -48,12 +65,7 @@ int ReadTransforms( std::string input , std::string output )
 {
   typedef itk::AffineTransform< U , 3 > InputAffineType ;
   typedef itk::Rigid3DTransform< U > InputRigidType ;
-  #if (ITK_VERSION_MAJOR == 4 && ITK_VERSION_MINOR >= 5) || ITK_VERSION_MAJOR> 4
-  typename itk::TransformFileReaderTemplate< U >::Pointer transformFile =
-     itk::TransformFileReaderTemplate< U >::New() ;
-  #else
   itk::TransformFileReader::Pointer transformFile = itk::TransformFileReader::New() ;
-  #endif
   transformFile->SetFileName( input.c_str() ) ;
   transformFile->Update() ;
   typename InputAffineType::Pointer affine ;
@@ -80,16 +92,37 @@ int ReadTransforms( std::string input , std::string output )
   }
 }
 
+#endif
+
 int Convert( int argc , char* argv[] )
 {
   if( argc != 5 )
   {
-    std::cout<< argv[ 0 ] << " " << argv[ 1 ] << " inputTransform outputTransform f2d/f2d" << std::endl ;
+    std::cout<< argv[ 0 ] << " " << argv[ 1 ] << " inputTransform outputTransform f2d|2d|d2f|2f" << std::endl ;
     return 1 ;
   }
   std::string input = argv[ 2 ] ;
   std::string output = argv[ 3 ] ;
   std::string type = argv[ 4 ] ;
+  #if (ITK_VERSION_MAJOR == 4 && ITK_VERSION_MINOR >= 5) || ITK_VERSION_MAJOR> 4
+  //We keep backward compatibility but really we just save the input transform in
+  //double or float, without looking at the input format, so we added the new flags "2d" and "2f"
+  //to reflect this modification.
+  if( type == "f2d" || type == "2d" )
+  {
+    return ConvertTransforms< double >( input , output ) ;
+  }
+  else if( type == "d2f" || type == "2f" )
+  {
+    return ConvertTransforms< float >( input , output ) ;
+  }
+  else
+  {
+    std::cerr << "Argument 5 is : " << type << std::endl ;
+    std::cerr << "Must be either 'f2d', '2d', 'd2f' or '2f'" << std::endl ;
+    return -1 ;
+  }
+  #else
   if( !type.compare( "f2d" ) )
   {
     return ReadTransforms< float , double >( input , output ) ;
@@ -104,4 +137,5 @@ int Convert( int argc , char* argv[] )
     std::cerr << "Must be either 'f2d' or 'd2f'" << std::endl ;
     return -1 ;
   }
+  #endif
 }
